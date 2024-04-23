752118845c49e7fe4f4d8a5e2bdcd8c6b9d33bad
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-04-23 04:49:28 -0500

    marking Name property of the Product class as nullable

--- WinFormsApp/Product.cs
+++ WinFormsApp/Product.cs
@@ -4,12 +4,12 @@ namespace WinFormsApp
 {
     public class Product
     {
-        public String Name { get; set; }
+        public String? Name { get; set; }
         public int Price { get; set; }
 
         public override String ToString()
         {
-            return Name;
+            return Name!;
         }
     }
 }
