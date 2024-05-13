0bcd4f33b705f2517720e27fff2e7f6eac79ea35
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-05-12 23:55:11 -0500

    adding a comment

--- ConsoleAppInteractDatabase/Program.cs
+++ ConsoleAppInteractDatabase/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleAppDatabaseAccess
 {
     public class Program
     {
+        // Main only for testing purposes.
         public static void Main(String[] args)
         {
         }
