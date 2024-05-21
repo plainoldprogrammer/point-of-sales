34e2bb888c035256655b83eaea321a1ee3153147
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-05-21 14:52:36 -0500

    adjusting the title of the settings window

--- WinFormsApp/FormSettings.Designer.cs
+++ WinFormsApp/FormSettings.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormSettings";
+            SuspendLayout();
+            // 
+            // FormSettings
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Name = "FormSettings";
+            Text = "Settings";
+            ResumeLayout(false);
         }
 
         #endregion
