3cc372058c0863413a18416cdc388c9def5b2f89
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-06-10 23:23:37 -0500

    adding the Message property to the FormAbout window

--- WinFormsApp/FormAbout.cs
+++ WinFormsApp/FormAbout.cs
@@ -1,9 +1,12 @@
 ﻿using System.Windows.Forms;
+using System;
 
 namespace WinFormsApp
 {
     public partial class FormAbout : Form
     {
+        private String Message { get; set; }
+
         public FormAbout()
         {
             InitializeComponent();
