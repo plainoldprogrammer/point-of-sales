cfecffc8ec623ad3e9523a6c597095b59050a019
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-06-13 00:03:24 -0500

    adding the ReleaseVersion property to the FormAbout window

--- WinFormsApp/FormAbout.cs
+++ WinFormsApp/FormAbout.cs
@@ -8,6 +8,7 @@ namespace WinFormsApp
         private String Message { get; set; }
         private String Author { get; set; }
         private String ReleaseDate { get; set; }
+        private String ReleaseVersion { get; set; }
 
         public FormAbout()
         {
