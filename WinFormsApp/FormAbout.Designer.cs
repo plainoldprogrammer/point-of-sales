90a01480fff2def221c7ad7ae57a9873fe21ccd4
PlainOldProgrammer <plainoldprogrammer@gmail.com>
2024-06-30 12:24:58 -0500

    renaming the label of button1 in the about window

--- WinFormsApp/FormAbout.Designer.cs
+++ WinFormsApp/FormAbout.Designer.cs
@@ -33,19 +33,21 @@
             // 
             // button1
             // 
-            button1.Location = new System.Drawing.Point(114, 212);
+            button1.Location = new System.Drawing.Point(171, 318);
+            button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(94, 29);
+            button1.Size = new System.Drawing.Size(141, 44);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Ok";
             button1.UseVisualStyleBackColor = true;
             // 
             // FormAbout
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(333, 272);
+            ClientSize = new System.Drawing.Size(500, 408);
             Controls.Add(button1);
+            Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             Name = "FormAbout";
             Text = "FormAbout";
             ResumeLayout(false);
